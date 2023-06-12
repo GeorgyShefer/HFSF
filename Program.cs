8331143436fd1456626d685d972c92f55a64e526
GeorgyShefer <132808044+GeorgyShefer@users.noreply.github.com>
2023-06-12 16:04:33 +0400

    Модуль 8 -  первое задание

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            DirectoryInfo necessaryDelete = new DirectoryInfo("C:\\Users\\user\\Desktop\\FoldersForDelete");
+            string folder = "C:\\Users\\user\\Desktop\\FoldersForDelete";
+
+            Console.WriteLine(TimeSpan.FromMinutes(30));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
